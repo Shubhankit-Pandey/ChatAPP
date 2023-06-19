368beeec462beff1444ed902251e4c42a70012ae
Shubhankit-Pandey <shubhankitpandey15@gmail.com>
2023-06-20 00:17:46 +0530

    initial setup

---
+++ lib/pages/signin.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class signIn extends StatefulWidget {
+  const signIn({super.key});
+
+  @override
+  State<signIn> createState() => _signInState();
+}
+
+class _signInState extends State<signIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("SignIn Page"),
+        alignment: Alignment.center,
+      ),
+    );
+  }
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import './signin.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 2000), () => {});
+    Navigator.pushAndRemoveUntil(context,
+        MaterialPageRoute(builder: (context) => signIn()), (route) => true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("Splash Screen"),
+        ),
+      ),
+    );
+  }
+}
